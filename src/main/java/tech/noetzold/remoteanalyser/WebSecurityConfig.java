7c314931e2c7fa70e0beaab03d4af7b2e5c4392f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-04-17 11:21:10 -0300

    Update WebSecurityConfig.java

--- src/main/java/tech/noetzold/remoteanalyser/WebSecurityConfig.java
+++ src/main/java/tech/noetzold/remoteanalyser/WebSecurityConfig.java
@@ -1,54 +1,49 @@
 package tech.noetzold.remoteanalyser;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import tech.noetzold.remoteanalyser.util.LoginApiService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource dataSource;
+	private UserDetailsService userDetailsService;
 
-	@Autowired
-	private LoginApiService loginProp;
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests()
-		.antMatchers("/login/**")
-			.permitAll()
-		.anyRequest()
-			.authenticated()
-		.and()
-		.formLogin(form -> form
-            .loginPage("/login")
-            .defaultSuccessUrl("/home", true)
-            .permitAll()
-        )
-		.logout(logout -> {
-			logout.logoutUrl("/logout")
-				.logoutSuccessUrl("/login");
-		}).csrf().disable();
+				.authorizeRequests()
+				.antMatchers("/css/**", "/js/**", "/resources/**", "/error").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/home")
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll();
 	}
 
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-				.inMemoryAuthentication()
-				.withUser(loginProp.getLogin()).password("{noop}"+loginProp.getPassword()).roles("ADMIN")
-				.and().withUser("user").password("{noop}user").roles("USER");
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
+	}
 
-
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
 	}
 
 }
