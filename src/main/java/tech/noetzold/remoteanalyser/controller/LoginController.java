6eea5231238f1a41469aec41d1d15d1efdaaf970
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-04-17 11:20:26 -0300

    Update LoginController.java

--- src/main/java/tech/noetzold/remoteanalyser/controller/LoginController.java
+++ src/main/java/tech/noetzold/remoteanalyser/controller/LoginController.java
@@ -1,8 +1,12 @@
 package tech.noetzold.remoteanalyser.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tech.noetzold.remoteanalyser.model.UserImp;
+import tech.noetzold.remoteanalyser.repository.UserRepository;
 
 @Controller
 public class LoginController {
