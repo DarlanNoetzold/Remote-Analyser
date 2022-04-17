82c4e1013b31d829355cee7dba4b60424be6ca68
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-04-17 11:20:57 -0300

    Update RemoteanalyserApplication.java

--- src/main/java/tech/noetzold/remoteanalyser/RemoteanalyserApplication.java
+++ src/main/java/tech/noetzold/remoteanalyser/RemoteanalyserApplication.java
@@ -1,9 +1,13 @@
 package tech.noetzold.remoteanalyser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tech.noetzold.remoteanalyser.model.UserImp;
+import tech.noetzold.remoteanalyser.repository.UserRepository;
 import tech.noetzold.remoteanalyser.util.LoginApiService;
 
 @EnableFeignClients
@@ -13,5 +17,4 @@ public class RemoteanalyserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RemoteanalyserApplication.class, args);
 	}
-
 }
