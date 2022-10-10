90e330380eefd889c1e73d7fbe219aebda25c2c2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-10-10 06:48:57 -0300

    Create MaliciousWebsiteService.java

---
+++ src/main/java/tech/noetzold/remoteanalyser/service/MaliciousWebsiteService.java
@@ -0,0 +1,23 @@
+package tech.noetzold.remoteanalyser.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.remoteanalyser.model.MaliciousProcess;
+import tech.noetzold.remoteanalyser.model.MaliciousWebsite;
+
+import java.util.List;
+
+@Service
+@FeignClient(url= "https://spyware-api.herokuapp.com" , name = "spyware")
+public interface MaliciousWebsiteService {
+
+    @PostMapping("/website/save")
+    void saveMaliciousWebsite(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody MaliciousWebsite maliciousWebsite);
+
+    @GetMapping("/website/getAll")
+    List<MaliciousWebsite> buscaMaliciousWebsite(@RequestHeader(value = "Authorization", required = true) String authorizationHeader);
+
+    @GetMapping("/website/remover/{id}")
+    void removeMaliciousWebsite(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @PathVariable("id") Long id);
+}
