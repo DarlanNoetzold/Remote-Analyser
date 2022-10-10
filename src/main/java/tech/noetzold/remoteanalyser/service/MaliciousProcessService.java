e7f74cd350c04e03f824a5a7c6ed667790377978
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-10-10 06:48:55 -0300

    Create MaliciousProcessService.java

---
+++ src/main/java/tech/noetzold/remoteanalyser/service/MaliciousProcessService.java
@@ -0,0 +1,23 @@
+package tech.noetzold.remoteanalyser.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.remoteanalyser.model.MaliciousPort;
+import tech.noetzold.remoteanalyser.model.MaliciousProcess;
+
+import java.util.List;
+
+@Service
+@FeignClient(url= "https://spyware-api.herokuapp.com" , name = "spyware")
+public interface MaliciousProcessService {
+
+    @PostMapping("/process/save")
+    void saveMaliciousProcess(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody MaliciousProcess maliciousProcess);
+
+    @GetMapping("/process/getAll")
+    List<MaliciousProcess> buscaMaliciousProcess(@RequestHeader(value = "Authorization", required = true) String authorizationHeader);
+
+    @GetMapping("/process/remover/{id}")
+    void removeMaliciousProcess(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @PathVariable("id") Long id);
+}
