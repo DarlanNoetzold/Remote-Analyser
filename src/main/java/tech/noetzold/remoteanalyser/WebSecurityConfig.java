8724bc269baad673993ab662b719ea894633edd7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-10-30 10:21:46 -0300

    Update WebSecurityConfig.java

--- src/main/java/tech/noetzold/remoteanalyser/WebSecurityConfig.java
+++ src/main/java/tech/noetzold/remoteanalyser/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package tech.noetzold.remoteanalyser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/css/**", "/js/**", "/resources/**", "/error", "/home", "cadastro", "salvarUser").permitAll()
+				.antMatchers("/css/**", "/js/**", "/resources/**", "/error").permitAll()
+				.antMatchers(HttpMethod.POST, "/*").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
