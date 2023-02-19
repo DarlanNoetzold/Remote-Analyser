f8205bb94c6d10c194274a699d3efba6f16e10d3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-18 21:02:58 -0300

    Update MaliciousPortService.java

--- src/main/java/tech/noetzold/remoteanalyser/service/MaliciousPortService.java
+++ src/main/java/tech/noetzold/remoteanalyser/service/MaliciousPortService.java
@@ -8,7 +8,7 @@ import tech.noetzold.remoteanalyser.model.MaliciousPort;
 import java.util.List;
 
 @Service
-@FeignClient(url= "https://spyware-api-production.up.railway.app" , name = "spyware")
+@FeignClient(url= "http://localhost:8091" , name = "spyware")
 public interface MaliciousPortService {
 
     @PostMapping("/port/save")
