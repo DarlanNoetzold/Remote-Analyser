f78d37972cebfa40a6096263e425805738f46082
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-29 08:09:16 -0300

    Update UserImp.java

--- src/main/java/tech/noetzold/remoteanalyser/model/UserImp.java
+++ src/main/java/tech/noetzold/remoteanalyser/model/UserImp.java
@@ -1,14 +1,15 @@
 package tech.noetzold.remoteanalyser.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
@@ -25,52 +26,78 @@ public class UserImp implements UserDetails {
 	private String password;
 	private Boolean enabled;
 
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+
 	public UserImp() {
 
 	}
-	public UserImp(String username, String password) {
+
+	public UserImp(String username, String password, List<String> roles) {
 		this.username = username;
 		this.password = password;
+		this.roles = roles;
 	}
 
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Boolean getEnabled() {
 		return enabled;
 	}
+
 	public void setEnabled(Boolean enabled) {
 		this.enabled = enabled;
 	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for (String role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role));
+		}
+		return authorities;
 	}
+
 	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
+
 	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
+
 	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
+
 	@Override
 	public boolean isEnabled() {
 		return true;
 	}
-	
-	
+
 }
