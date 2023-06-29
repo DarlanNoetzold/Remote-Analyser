24f11233d24e840c4acb08b1c6f70abd421d2fa6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-29 08:09:20 -0300

    Update LoginController.java

--- src/main/java/tech/noetzold/remoteanalyser/controller/LoginController.java
+++ src/main/java/tech/noetzold/remoteanalyser/controller/LoginController.java
@@ -1,9 +1,13 @@
 package tech.noetzold.remoteanalyser.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
@@ -12,6 +16,20 @@ public class LoginController {
 	public String login() {
 		return "login";
 	}
+
+	@GetMapping
+	@RequestMapping("/login-success")
+	public RedirectView successLogin(@AuthenticationPrincipal UserDetails userDetails) {
+		boolean isAdmin = userDetails.getAuthorities().stream()
+				.anyMatch(authority -> authority.equals(new SimpleGrantedAuthority("ROLE_admin")));
+
+		if (isAdmin) {
+			return new RedirectView("/home");
+		} else {
+			return new RedirectView("/user");
+		}
+	}
+
 	@GetMapping
 	@RequestMapping("/login-error")
 	public ModelAndView loginError() {
